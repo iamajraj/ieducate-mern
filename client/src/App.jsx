5d50748f98eae6a393ef03ff2463535798dc6189
iamajraj <akmalraj07@gmail.com>
2023-06-23 15:47:52 +0600

    code split using lazy()

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-import AuthProvider from "./context/AuthProvider";
-import router from "./routes";
+import React, { Suspense } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import AuthProvider from './context/AuthProvider';
+import router from './routes';
 
 const App = () => {
-    return (
-        <AuthProvider>
-            <RouterProvider router={router} />
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <Suspense fallback={null}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </AuthProvider>
+  );
 };
 
 export default App;
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,53 +1,133 @@
-import { useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import { createBrowserRouter, Navigate, useNavigate } from 'react-router-dom';
 import MainLayout from '../components/MainLayout';
 
-import Admins from '../pages/admin/Admin/Admins';
-import AddAdmin from '../pages/admin/Admin/AddAdmin';
-import AdminLogin from '../pages/admin/AdminLogin';
+// import Admins from '../pages/admin/Admin/Admins';
+// import AddAdmin from '../pages/admin/Admin/AddAdmin';
+// import AdminLogin from '../pages/admin/AdminLogin';
+// import TeacherLogin from '../pages/teacher/TeacherLogin';
+// import AddTeacher from '../pages/admin/Teacher/AddTeacher';
+// import TeacherAttendance from '../pages/admin/Teacher/TeacherAttendance';
+// import SetTeacherAttendance from '../pages/admin/Teacher/SetTeacherAttendance';
+// import Teachers from '../pages/admin/Teacher/Teachers';
+// import AllStudents from '../pages/teacher/Report/AllStudents';
+// import CreateGeneralReport from '../pages/teacher/Report/CreateGeneralReport';
+// import CreateTestReport from '../pages/teacher/Report/CreateTestReport';
+// import AddStudent from '../pages/admin/Student/AddStudent';
+// import Students from '../pages/admin/Student/Students';
+// import AddAnnouncement from '../pages/admin/Announcements/AddAnnouncement';
+// import SendAnnouncement from '../pages/admin/Announcements/SendAnnouncement';
+// import FeesBilling from '../pages/admin/feesbillings/FeesBilling';
+// import FeesStudents from '../pages/admin/feesbillings/FeesStudents';
+// import Announcements from '../pages/admin/Announcements/Announcement';
+// import Home from '../pages/Home';
+// import EditStudent from '../pages/admin/Student/EditStudent';
+// import ViewFees from '../pages/admin/feesbillings/ViewFees';
+// import CredentialsUpdate from '../pages/teacher/CredentialsUpdate';
+// import TrackAttendance from '../pages/teacher/TrackAttendance';
+// import StudentReports from '../pages/teacher/Report/StudentReports';
+// import EditGeneralReport from '../pages/teacher/Report/EditGeneralReport';
+// import Dashboard from '../pages/admin/Dashboard';
+// import AllFees from '../pages/admin/feesbillings/AllFees';
+// import EditTestReport from '../pages/teacher/Report/EditTestReport';
+// import TeacherDashboard from '../pages/teacher/TeacherDashboard';
+// import AutoLogout from '../components/AutoLogout';
+// import StudentDashboard from '../pages/student/StudentDashboard';
+// import StudentAnnouncement from '../pages/student/Announcement/StudentAnnouncement';
+// import StudentGeneralReport from '../pages/student/GeneralReport/StudentGeneralReport';
+// import StudentTermsReport from '../pages/student/TermsReport/StudentTermsReport';
+// import StudentTermsReportView from '../pages/student/TermsReport/StudentTermsReportView';
+// import StudentInvoice from '../pages/student/Invoice/StudentInvoice';
+// import StudentInvoiceView from '../pages/student/Invoice/StudentInvoiceView';
+// import ProfileSetting from '../pages/student/Setting/ProfileSetting';
+// import StudentLayout from '../components/StudentLayout';
+// import PageNotFound from '../components/PageNotFound';
+const StudentLogin = lazy(() => import('../pages/student/StudentLogin'));
 
-import TeacherLogin from '../pages/teacher/TeacherLogin';
-import AddTeacher from '../pages/admin/Teacher/AddTeacher';
-import TeacherAttendance from '../pages/admin/Teacher/TeacherAttendance';
-import SetTeacherAttendance from '../pages/admin/Teacher/SetTeacherAttendance';
-import Teachers from '../pages/admin/Teacher/Teachers';
-
-import AllStudents from '../pages/teacher/Report/AllStudents';
-import CreateGeneralReport from '../pages/teacher/Report/CreateGeneralReport';
-import CreateTestReport from '../pages/teacher/Report/CreateTestReport';
-
-import AddStudent from '../pages/admin/Student/AddStudent';
-import Students from '../pages/admin/Student/Students';
-
-import AddAnnouncement from '../pages/admin/Announcements/AddAnnouncement';
-import SendAnnouncement from '../pages/admin/Announcements/SendAnnouncement';
-
-import FeesBilling from '../pages/admin/feesbillings/FeesBilling';
-import FeesStudents from '../pages/admin/feesbillings/FeesStudents';
-import Announcements from '../pages/admin/Announcements/Announcement';
-import Home from '../pages/Home';
-import EditStudent from '../pages/admin/Student/EditStudent';
-import ViewFees from '../pages/admin/feesbillings/ViewFees';
-import CredentialsUpdate from '../pages/teacher/CredentialsUpdate';
-import TrackAttendance from '../pages/teacher/TrackAttendance';
-import StudentReports from '../pages/teacher/Report/StudentReports';
-import EditGeneralReport from '../pages/teacher/Report/EditGeneralReport';
-import Dashboard from '../pages/admin/Dashboard';
-import AllFees from '../pages/admin/feesbillings/AllFees';
-import EditTestReport from '../pages/teacher/Report/EditTestReport';
-import TeacherDashboard from '../pages/teacher/TeacherDashboard';
-import AutoLogout from '../components/AutoLogout';
-import StudentLogin from '../pages/student/StudentLogin';
-import StudentDashboard from '../pages/student/StudentDashboard';
-import StudentAnnouncement from '../pages/student/Announcement/StudentAnnouncement';
-import StudentGeneralReport from '../pages/student/GeneralReport/StudentGeneralReport';
-import StudentTermsReport from '../pages/student/TermsReport/StudentTermsReport';
-import StudentTermsReportView from '../pages/student/TermsReport/StudentTermsReportView';
-import StudentInvoice from '../pages/student/Invoice/StudentInvoice';
-import StudentInvoiceView from '../pages/student/Invoice/StudentInvoiceView';
-import ProfileSetting from '../pages/student/Setting/ProfileSetting';
-import StudentLayout from '../components/StudentLayout';
-import PageNotFound from '../components/PageNotFound';
+const Admins = lazy(() => import('../pages/admin/Admin/Admins'));
+const AddAdmin = lazy(() => import('../pages/admin/Admin/AddAdmin'));
+const AdminLogin = lazy(() => import('../pages/admin/AdminLogin'));
+const TeacherLogin = lazy(() => import('../pages/teacher/TeacherLogin'));
+const AddTeacher = lazy(() => import('../pages/admin/Teacher/AddTeacher'));
+const TeacherAttendance = lazy(() =>
+  import('../pages/admin/Teacher/TeacherAttendance')
+);
+const SetTeacherAttendance = lazy(() =>
+  import('../pages/admin/Teacher/SetTeacherAttendance')
+);
+const Teachers = lazy(() => import('../pages/admin/Teacher/Teachers'));
+const AllStudents = lazy(() => import('../pages/teacher/Report/AllStudents'));
+const CreateGeneralReport = lazy(() =>
+  import('../pages/teacher/Report/CreateGeneralReport')
+);
+const CreateTestReport = lazy(() =>
+  import('../pages/teacher/Report/CreateTestReport')
+);
+const AddStudent = lazy(() => import('../pages/admin/Student/AddStudent'));
+const Students = lazy(() => import('../pages/admin/Student/Students'));
+const AddAnnouncement = lazy(() =>
+  import('../pages/admin/Announcements/AddAnnouncement')
+);
+const SendAnnouncement = lazy(() =>
+  import('../pages/admin/Announcements/SendAnnouncement')
+);
+const FeesBilling = lazy(() =>
+  import('../pages/admin/feesbillings/FeesBilling')
+);
+const FeesStudents = lazy(() =>
+  import('../pages/admin/feesbillings/FeesStudents')
+);
+const Announcements = lazy(() =>
+  import('../pages/admin/Announcements/Announcement')
+);
+const Home = lazy(() => import('../pages/Home'));
+const EditStudent = lazy(() => import('../pages/admin/Student/EditStudent'));
+const ViewFees = lazy(() => import('../pages/admin/feesbillings/ViewFees'));
+const CredentialsUpdate = lazy(() =>
+  import('../pages/teacher/CredentialsUpdate')
+);
+const TrackAttendance = lazy(() => import('../pages/teacher/TrackAttendance'));
+const StudentReports = lazy(() =>
+  import('../pages/teacher/Report/StudentReports')
+);
+const EditGeneralReport = lazy(() =>
+  import('../pages/teacher/Report/EditGeneralReport')
+);
+const Dashboard = lazy(() => import('../pages/admin/Dashboard'));
+const AllFees = lazy(() => import('../pages/admin/feesbillings/AllFees'));
+const EditTestReport = lazy(() =>
+  import('../pages/teacher/Report/EditTestReport')
+);
+const TeacherDashboard = lazy(() =>
+  import('../pages/teacher/TeacherDashboard')
+);
+const AutoLogout = lazy(() => import('../components/AutoLogout'));
+const StudentDashboard = lazy(() =>
+  import('../pages/student/StudentDashboard')
+);
+const StudentAnnouncement = lazy(() =>
+  import('../pages/student/Announcement/StudentAnnouncement')
+);
+const StudentGeneralReport = lazy(() =>
+  import('../pages/student/GeneralReport/StudentGeneralReport')
+);
+const StudentTermsReport = lazy(() =>
+  import('../pages/student/TermsReport/StudentTermsReport')
+);
+const StudentTermsReportView = lazy(() =>
+  import('../pages/student/TermsReport/StudentTermsReportView')
+);
+const StudentInvoice = lazy(() =>
+  import('../pages/student/Invoice/StudentInvoice')
+);
+const StudentInvoiceView = lazy(() =>
+  import('../pages/student/Invoice/StudentInvoiceView')
+);
+const ProfileSetting = lazy(() =>
+  import('../pages/student/Setting/ProfileSetting')
+);
+const StudentLayout = lazy(() => import('../components/StudentLayout'));
+const PageNotFound = lazy(() => import('../components/PageNotFound'));
 
 const AdminProtectedRoute = ({ children }) => {
   const navigate = useNavigate();
